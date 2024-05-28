ee8f4020652d719d06b4bd335eb669f0445130c1
vucapk9 <nguyenvanvu642004@gmail.com>
2024-05-28 19:35:20 +0700

    Lab1.1

---
+++ Lab1.1/Lab1.1/Controllers/NvvHomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1._1.Controllers
+{
+    public class NvvHomeController : Controller
+    {
+        // GET: /Home/Index
+        public ActionResult Index()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.message = "Chào mừng bạn đến với ASP.NET MVC 5";
+            return View();
+        }
+        //GET: /Home/About
+        public ActionResult About()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.message = "Đây là trang About";
+            return View();
+        }
+    }
+}
